72c4df3418479390315dd988c17101d752c82a95
Suraj Patil <surajpatil110110@gmail.com>
2023-12-29 17:11:23 +0530

    Initial Commit

---
+++ src/electricity/billing/system/Conn.java
@@ -0,0 +1,15 @@
+
+package electricity.billing.system;
+import java.sql.*;
+public class Conn {
+     Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "Suraj@2002");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
